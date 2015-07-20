855cf957b231eef87ee6b6f41f4e0a650fba065f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-07-20 16:00:02 +0300

    no message

--- Block/Backend/Buttons.php
+++ Block/Backend/Buttons.php
@@ -1,3 +1,19 @@
 <?php
 namespace Df\Login\Block\Backend;
-class Buttons extends \Magento\Backend\Block\Template {}
+class Buttons extends \Magento\Backend\Block\Template {
+	/**
+	 * @param \Magento\Backend\Block\Template\Context $context
+	 * @param \Magento\Framework\View\Page\Config $pageConfig
+	 * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
+	 */
+	public function __construct(
+		\Magento\Backend\Block\Template\Context $context
+		,\Magento\Framework\View\Page\Config $pageConfig
+		,\Magento\Framework\App\Config\ScopeConfigInterface $config
+	) {
+		$pageConfig->setMetadata(
+			'google-signin-client_id', $config->getValue('df_login/google/client_id')
+		);
+		parent::__construct($context);
+	}
+}
