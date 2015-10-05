e9d98b4c94c78ca5f7abd97211efab0802a0b067
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-10-05 07:11:03 +0300

    no message

--- Block/Backend/Metadata.php
+++ Block/Backend/Metadata.php
@@ -2,16 +2,11 @@
 namespace Dfe\Google\Block\Backend;
 class Metadata extends \Magento\Backend\Block\AbstractBlock {
 	/**
-	 * @param \Magento\Backend\Block\Context $context
-	 * @param \Magento\Framework\View\Page\Config $pageConfig
-	 * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
+	 * @override
+	 * @see \Magento\Backend\Block\AbstractBlock::_construct()
+	 * @return void
 	 */
-	public function __construct(
-		\Magento\Backend\Block\Context $context
-		,\Magento\Framework\View\Page\Config $pageConfig
-		,\Magento\Framework\App\Config\ScopeConfigInterface $config
-	) {
-		$pageConfig->setMetadata('google-signin-client_id', \Dfe\Google\Settings\Login::s()->clientId());
-		parent::__construct($context);
+	protected function _construct() {
+		df_metadata('google-signin-client_id', \Dfe\Google\Settings\Login::s()->clientId());
 	}
 }
