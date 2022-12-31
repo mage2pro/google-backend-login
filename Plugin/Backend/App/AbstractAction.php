28a3a58fa2211bbcd99c1d11a8de9e794dcc328d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-31 16:49:42 +0000

    refactoring

--- Plugin/Backend/App/AbstractAction.php
+++ Plugin/Backend/App/AbstractAction.php
@@ -25,28 +25,16 @@ class AbstractAction {
 		$token = $request->getParam('id_token'); /** @var string|null $token */
 		if ($isOAuthLogin && !df_backend_auth()->isLoggedIn() && $token) {
 			/** https://developers.google.com/identity/sign-in/web/backend-auth */
-			/** @var array(string => string)|null $googleResponse */
-			/**
-			 * 2015-11-27
-			 * Обратите внимание, что для использования @uses file_get_contents
-			 * с адресами https требуется расширение php_openssl интерпретатора PHP,
-			 * однако оно является системным требованием Magento 2:
-			 * http://devdocs.magento.com/guides/v2.0/install-gde/system-requirements.html#required-php-extensions
-			 * Поэтому мы вправе использовать здесь @uses file_get_contents
-			 */
-			$googleResponse = df_http_json('https://www.googleapis.com/oauth2/v3/tokeninfo', [
-				'id_token' => $token
-			]);
-			if ($googleResponse) {
-				$email = dfa($googleResponse, 'email'); /** @var string $email */
-				# The value of aud in the ID token is equal to one of your app's client IDs.
+			/** @var array(string => string) $res */
+			if ($res = df_http_json('https://www.googleapis.com/oauth2/v3/tokeninfo', ['id_token' => $token], 0, '')) {
+				$email = dfa($res, 'email'); /** @var string $email */
+				# 1) The value of aud in the ID token is equal to one of your app's client IDs.
 				# This check is necessary to prevent ID tokens issued to a malicious app
 				# being used to access data about the same user on your app's backend server.
-				#
-				# Говоря простым языком, нам надо убедиться,
-				# что админгистратор авторизован именно в нашем приложении, а не в каком-то ещё.
+				# 2) Говоря простым языком, нам надо убедиться,
+				# что администратор авторизован именно в нашем приложении, а не в каком-то ещё.
 				# https://developers.google.com/identity/sign-in/web/backend-auth#verify-the-integrity-of-the-id-token
-				$clientId = dfa($googleResponse, 'aud'); /** @var string $clientId */
+				$clientId = dfa($res, 'aud'); /** @var string $clientId */
 				$expectedClientId = S::s()->clientId(); /** @var string $expectedClientId */
 				if ($email && $clientId === $expectedClientId) {
 					df_backend_auth()->loginByEmail($email);
